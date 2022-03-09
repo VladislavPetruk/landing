f237681bbbb0ecbd6bf41ad01e1662f302246179
VladislavPetruk <vladiislav.petruk@gmail.com>
2022-03-09 18:52:41 +0200

    dev

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -2,13 +2,11 @@ import GraphTabs from 'graph-tabs';
 
 const tabs1 = new GraphTabs('tab', {
   isChanged: (tabs) => {
-    console.log(tabs);
   }
 });
 
 const tabs2 = new GraphTabs('tab2', {
   isChanged: (tabs) => {
-    console.log(tabs);
   }
 });
 
